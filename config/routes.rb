fcca9dcab9ae4dd98a01436ee308d195a00899e3
Egor1007-del <hyinm2012@yandex.ru>
2026-05-27 18:56:27 +0200

    add model Category and fixtures

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root "web/home#index"
-
   scope module: :web do
+    root "home#index"
+
     post "auth/:provider", to: "auth#auth_request", as: :auth_request
     match "auth/:provider/callback", to: "auth#callback", as: :callback_auth, via: %i[get post]
     delete "logout", to: "auth#destroy", as: :logout
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_25_214751) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_27_165145) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
