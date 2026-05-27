3e0a566cf81a6e0b59370c41f613cd1c7fb3a7e8
Egor1007-del <hyinm2012@yandex.ru>
2026-05-27 19:20:04 +0200

    add model Bulletin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_many :bulletins, inverse_of: :category, dependent: :destroy
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_27_165145) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_27_170949) do
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bulletins_on_category_id"
+    t.index ["user_id"], name: "index_bulletins_on_user_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -18,10 +29,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_27_165145) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "bulletins", "categories"
+  add_foreign_key "bulletins", "users"
 end
