a0ccac6c9e26d9bf5470b6d7ff42fcca4ba67179
Egor1007-del <hyinm2012@yandex.ru>
2026-05-27 19:24:12 +0200

    added validation

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,4 +1,7 @@
 class Bulletin < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title,  presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 1000 }
 end
