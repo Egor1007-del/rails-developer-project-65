748aa9f3b405411b269eb828a401830688caff4a
Egor1007-del <hyinm2012@yandex.ru>
2026-05-27 19:39:17 +0200

    added active_storage for image

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,7 +1,22 @@
 class Bulletin < ApplicationRecord
+  MAX_IMAGE_SIZE = 5.megabytes
+
   belongs_to :user
   belongs_to :category
 
+  has_one_attached :image
+
   validates :title,  presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1000 }
+  validates :image, presences: true
+  validate :image_size
+
+  private
+
+  def image_size
+    return unless image.attached?
+    return if image.blob.byte_size <= MAX_IMAGE_SIZE
+
+    errors.add(:image, :too_large)
+  end
 end
