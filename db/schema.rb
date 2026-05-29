0d54e576d29af7580da2baecc5429bc7cb76067f
Egor1007-del <hyinm2012@yandex.ru>
2026-05-29 22:42:02 +0200

    added admin field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_27_172648) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_29_203711) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_27_172648) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
