4da53640762b3320c830b5e3427a09d19d0d69de
Egor1007-del <hyinm2012@yandex.ru>
2026-05-31 21:29:53 +0200

    added pagination to the main page

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -2,7 +2,10 @@ module Web
   class HomeController < ApplicationController
     def index
       @q = Bulletin.published.ransack(params[:q])
-      @bulletins = @q.result(distinct: true).includes(:category, :user).order(created_at: :desc)
+      @bulletins = @q.result(distinct: true)
+        .includes(:category, :user)
+        .order(created_at: :desc)
+        .page(params[:page]).per(12)
     end
   end
 end
