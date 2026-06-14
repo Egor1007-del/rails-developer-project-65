60f0530e4cfd5fd772f3c3930066dd048e4e126f
Egor1007-del <hyinm2012@yandex.ru>
2026-06-14 19:03:30 +0300

    added an image validation test

--- test/models/bulletin_test.rb
+++ test/models/bulletin_test.rb
@@ -2,15 +2,26 @@ require "test_helper"
 
 class BulletinTest < ActiveSupport::TestCase
   test "requires image" do
-    bulletin = Bulletin.new(
-      title: "Title",
-      description: "Description",
-      category: categories(:one),
-      user: users(:regular)
-    )
+    bulletin = build_bulletin
 
     assert_not bulletin.valid?
-    assert bulletin.errors.added?(:image, :blank)
+    assert_includes bulletin.errors.details[:image].pluck(:error), :blank
+  end
+
+  test "requires image with allowed content type" do
+    bulletin = build_bulletin
+    bulletin.image.attach(io: StringIO.new("plain text"), filename: "image.txt", content_type: "text/plain")
+
+    assert_not bulletin.valid?
+    assert_includes bulletin.errors.details[:image].pluck(:error), :content_type_invalid
+  end
+
+  test "requires image up to 5 megabytes" do
+    bulletin = build_bulletin
+    bulletin.image.attach(io: StringIO.new("x" * 6.megabytes), filename: "image.png", content_type: "image/png")
+
+    assert_not bulletin.valid?
+    assert_includes bulletin.errors.details[:image].pluck(:error), :file_size_not_less_than_or_equal_to
   end
 
   test "allows moderation from draft" do
@@ -23,5 +34,16 @@ class BulletinTest < ActiveSupport::TestCase
     bulletin = bulletins(:draft)
 
     assert_not bulletin.may_publish?
+  end
+
+  private
+
+  def build_bulletin
+    Bulletin.new(
+      title: "Title",
+      description: "Description",
+      category: categories(:one),
+      user: users(:regular)
+    )
   end
 end
