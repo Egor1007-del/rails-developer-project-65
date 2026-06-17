36a94bdfb8e521f99ba1372c5551acd956f9469f
Egor1007-del <hyinm2012@yandex.ru>
2026-06-18 01:31:57 +0300

    transferring logic to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
-
-  rescue_from Pundit::NotAuthorizedError do
-    redirect_to root_path, alert: t("pundit.not_authorized")
-  end
 end
--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,5 +1,10 @@
 module Web
   class ApplicationController < ::ApplicationController
+    include Pundit::Authorization
     include AuthManagement
+
+    rescue_from Pundit::NotAuthorizedError do
+      redirect_to root_path, alert: t("pundit.not_authorized")
+    end
   end
 end
